17a080609c98755f9f463c4d22b10a92849f38a3
vlammendeboy <warre.vanlancker@gmail.com>
2023-03-09 14:33:44 +0100

    coordinaten aangepast

--- src/move.py
+++ src/move.py
@@ -7,17 +7,17 @@ from tf_transformations import euler_from_quaternion
 from math import atan2
 import math
 from geometry_msgs.msg import Point
-n=0
-xlist= [4.0,-3.0,7.0,-9.0]
-ylist= [7.0,3.0,-2.0,9.0]
+
+n = 0
+coordinates = [[1.0, 1.0], [-2.0, 2.0], [2.0, -2.0], [-1.0, 1.0], [0.0, 0.0]]
 def Robotpos(msg):
     global x
     global y
     global theta
 
-    x= msg.pose.pose.position.x
-    y= msg.pose.pose.position.y
-    rot_q=msg.pose.pose.orientation #lees de positie en orientatie in van de odometry msg
+    x = msg.pose.pose.position.x
+    y = msg.pose.pose.position.y
+    rot_q = msg.pose.pose.orientation #lees de positie en orientatie in van de odometry msg
     
     (roll, pitch, theta)=euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
 
@@ -25,26 +25,26 @@ rclpy.init()
 
 
 
-node=rclpy.create_node('move')
-sub= node.create_subscription(
+node = rclpy.create_node('move')
+sub = node.create_subscription(
     Odometry, 'odom', Robotpos, 10
 )
-pub=node.create_publisher(Point,'goalxy',10)
-goal=Point()
-goal.x= 0.0
-goal.y=0.0
-xff=goal.x
-r= node.create_rate(4)
+pub = node.create_publisher(Point,'goalxy',10)
+goal = Point()
+goal.x = 0.0
+goal.y = 0.0
+r = node.create_rate(4)
 
 while rclpy.ok():
     
     rclpy.spin_once(node)
     inc_x = goal.x - x   #delta x
     inc_y = goal.y - y 
-    print("goal:(" + str(goal.x) + "," + str(goal.y) + ")")
-    if math.sqrt(inc_x*inc_x + inc_y*inc_y)<0.1: #stop als de robot bij het punt is
-        goal.x=xlist[n]
-        goal.y=ylist[n]
-        n=n+1
+    if math.sqrt(inc_x*inc_x + inc_y*inc_y) <= 0.1: #stop als de robot bij het punt is
+        goal.x = float(coordinates[n][0])
+        goal.y = float(coordinates[n][1])
+        n = n + 1
+        if n < len(coordinates):
+            print("goal:(" + str(goal.x) + "," + str(goal.y) + ")")
     pub.publish(goal)
     r.sleep
